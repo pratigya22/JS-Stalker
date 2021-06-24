e24e3d7370ad0586cebfc7871e334796bce4be71
Pratigya Srivastava <86208144+pratigya22@users.noreply.github.com>
2021-06-25 04:43:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var firstName = prompt ("What is your first name?");
+var lastName = prompt ("What is your last name?");
+var userAge = prompt ("What is your age?");
+console.log ("Nice to meet you, " + firstName + " " + lastName);
+console.log ("Age specified: " + userAge);
